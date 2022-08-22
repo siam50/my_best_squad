47320dd618ad6a0075f98d508ec3de29c13db773
siam50 <siamurrahman50@gmail.com>
2022-08-22 11:40:14 +0600

    five player select code done

--- js/apps.js
+++ js/apps.js
@@ -1,1 +1,20 @@
-console.log('im here');
+const selectButton = document.getElementsByClassName('btn-select');
+for (const select of selectButton) {
+    select.addEventListener('click', function (event) {
+        const olList = document.getElementById('ol-list');
+        if (olList.childNodes.length <= 5) {
+            event.target.setAttribute('disabled', 'disabled');
+            const playerName = event.target.parentNode.parentNode.childNodes[1].innerText;
+            const li = document.createElement('li');
+            li.innerText = playerName;
+            olList.appendChild(li);
+        }
+        else {
+            alert('You Cannot Select More Than five Players')
+        }
+
+    })
+}
+// if (event.target.innerText === 'SELECT') {
+        //     event.target.parentNode.removeChild(event.target)
+        // }
