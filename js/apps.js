04bb9eab7ee5a228deae67228902ff9e58443dab
siam50 <siamurrahman50@gmail.com>
2022-08-22 12:45:02 +0600

    player expenses calculation complete

--- js/apps.js
+++ js/apps.js
@@ -15,6 +15,39 @@ for (const select of selectButton) {
 
     })
 }
-// if (event.target.innerText === 'SELECT') {
-        //     event.target.parentNode.removeChild(event.target)
-        // }
+
+function getInputValue(inputId) {
+    const inputField = document.getElementById(inputId);
+    const inputValues = inputField.value;
+    const covertNumber = parseFloat(inputValues);
+    return covertNumber;
+}
+
+function getElementValue(elementId) {
+    const elementField = document.getElementById(elementId);
+    const elementValue = elementField.innerText;
+    const covertNumber = parseFloat(elementValue);
+    return covertNumber;
+}
+
+function setValue(elementId, value) {
+    const getElementField = document.getElementById(elementId);
+    getElementField.innerText = value;
+}
+
+document.getElementById('btn-calculate').addEventListener('click', function () {
+    const playerPrice = getInputValue('player-price');
+    const olList = document.getElementById('ol-list');
+    const olLength = olList.childNodes.length;
+    const totalPlayerExpenses = playerPrice * (olLength - 1);
+    console.log(totalPlayerExpenses);
+    setValue('player-expenses', totalPlayerExpenses);
+})
+
+document.getElementById('btn-total').addEventListener('click', function () {
+    const managerPrice = getInputValue('manager');
+    const coachPrice = getInputValue('coach');
+    const totalPlayerExpenses = getElementValue('player-expenses');
+    const totalCost = managerPrice + coachPrice + totalPlayerExpenses;
+    setValue('total', totalCost);
+})
